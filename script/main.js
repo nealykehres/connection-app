f25f7540fdf4224cf6021167ad8430e0db7c0478
Nealy Kehres <31978373+nealykehres@users.noreply.github.com>
2017-11-30 16:06:55 -0500

    commit 2

---
+++ script/main.js
@@ -0,0 +1,30 @@
+$("#cox").flip({
+  axis: 'y',
+  trigger: 'hover'
+});
+
+$("#att").flip({
+  axis: 'y',
+  trigger: 'hover'
+});
+
+$("#dish").flip({
+  axis: 'y',
+  trigger: 'hover'
+});
+
+$("#gator").flip({
+  axis: 'y',
+  trigger: 'hover'
+});
+
+
+$("#hotspots").flip({
+  axis: 'y',
+  trigger: 'hover'
+});
+
+$("#windstream").flip({
+  axis: 'y',
+  trigger: 'hover'
+});
---
+++ script/quiz.js
@@ -0,0 +1,293 @@
+(function() {
+    /*************************************************************************
+    This should be the full address to your quiz, in order for the result
+    codes to link to it.
+
+    *************************************************************************/
+    var QUIZ_URL = "http://www.example.com/a_quiz.html";
+
+    /*************************************************************************
+    This should be the full address to whatever folder on your website
+    directly contains your result images, ending in a forward slash.
+
+    *************************************************************************/
+    var RESULTS_FOLDER_URL = "http://www.example.com/a_folder/";
+
+    /*************************************************************************
+    The pairs between brackets should be the names of personality traits that
+    you use as the value attribute of the answer radio buttons. Each pair
+    should consist of two opposite traits, such as extravert and introvert or
+    stoic and emotional. You can have any number of trait pairs, although be
+    warned that each trait pair doubles the number of results.
+
+    *************************************************************************/
+    var TRAIT_PAIRS = [["urban", "rural"], ["lessthan55", "morethan55"],  ["onetothree", "fourormore"], ["yes", "no"]];
+
+    /*************************************************************************
+    Below, enter the possible final results for your quiz.
+
+    The string inside the brackets should consist of one trait from each of
+    the trait pairs defined above, in the order in which they're defined,
+    joined by ampersands. Thus, if the trait pairs are ["introvert",
+    "extravert"] and ["stoic", "emotional"], your results should be
+    "introvert&stoic", "introvert&emotional", "extravert&stoic" and
+    "extravert&emotional". You need to include one result for each possible
+    combination of traits - that means 2^n results if you have n trait pairs.
+    If that seems too much, this may not be the quiz for you.
+
+    Where it says "Result 1", "Result 2", etc., enter the full name of the
+    result, such as "Alakazam". Where it says "result_1.jpg", "result_2.jpg",
+    etc., enter the filename of the image the user should be presented with if
+    they get that result. Where it says "Text for the first result", "Text for
+    the second result", etc., enter the text the user should be presented with
+    if they get that result. (If you do not want them to get any text with
+    their result, you can erase those lines altogether.) Remember to enclose
+    these values in quotes.
+
+    It is also possible to leave out the name, image and text and instead add
+    a link property, like so:
+
+    RESULTS["introvert&stoic"] = {
+        link: "/quiz/alakazam.html"
+    };
+
+    If you do this, then the user will be redirected to the link for their
+    result when they finish the quiz. The link, like a regular HTML link, can
+    be a full URL or an absolute or relative path on your server.
+
+    Optionally, you can add extra results where "both" is used in place of a
+    trait in the result identifier, to represent a tie between the two
+    extremes. If you don't include a "both" result, the trait that comes first
+    in the definition of the trait pairs will take precedence (that is, if
+    you have no result for "both&stoic", then a user with a tie between
+    introvert and extravert but more stoic than emotional will get the result
+    "introvert&stoic" if "introvert" comes before "extravert" in the trait
+    pairs).
+
+    *************************************************************************/
+
+    var RESULTS = {};
+
+    RESULTS["rural&morethan55&fourormore&yes"] = {
+        name: "Windstream",
+        image: "result_1.jpg",
+        text: "Text for the first result"
+    };
+
+    RESULTS["rural&morethan55&fourormore&no"] = {
+        name: "Dish",
+        image: "result_2.jpg",
+        text: "Text for the second result"
+    };
+
+    RESULTS["rural&morethan55&onetothree&yes"] = {
+        name: "Windstream",
+        image: "result_3.jpg",
+        text: "Text for the third result"
+    };
+
+    RESULTS["rural&morethan55&onetothree&no"] = {
+        name: "Dish",
+        image: "result_4.jpg",
+        text: "Text for the fourth result"
+    };
+
+    RESULTS["rural&lessthan55&fourormore&yes"] = {
+        name: "Windstream",
+        image: "result_4.jpg",
+        text: "Text for the fourth result"
+          };
+
+    RESULTS["rural&lessthan55&fourormore&no"] = {
+            name: "Hotspot",
+            image: "result_4.jpg",
+            text: "Text for the fourth result"
+  };
+    RESULTS["rural&lessthan55&onetothree&yes"] = {
+            name: "Windstream",
+            image: "result_4.jpg",
+            text: "Text for the fourth result"
+  };
+    RESULTS["rural&lessthan55&onetothree&no"] = {
+            name: "Hotspot",
+            image: "result_4.jpg",
+            text: "Text for the fourth result"
+  };
+    RESULTS["urban&morethan55&fourormore&yes"] = {
+            name: "AT&T",
+            image: "result_4.jpg",
+            text: "Text for the fourth result"
+  };
+    RESULTS["urban&morethan55&fourormore&no"] = {
+           name: "COX",
+           image: "result_4.jpg",
+           text: "Text for the fourth result"
+  };
+   RESULTS["urban&morethan55&onetothree&yes"] = {
+          name: "AT&T",
+         image: "result_4.jpg",
+        text: "Text for the fourth result"
+  };
+  RESULTS["urban&morethan55&onetothree&no"] = {
+          name: "COX",
+          image: "result_4.jpg",
+          text: "Text for the fourth result"
+  };
+  RESULTS["urban&lessthan55&fourormore&yes"] = {
+            name: "AT&T",
+            image: "result_4.jpg",
+            text: "Text for the fourth result"
+  };
+    RESULTS["urban&lessthan55&fourormore&no"] = {
+              name: "COX",
+              image: "result_4.jpg",
+              text: "Text for the fourth result"
+  };
+     RESULTS["urban&lessthan55&onetothree&yes"] = {
+              name: "AT&T",
+              image: "result_4.jpg",
+              text: "Text for the fourth result"
+  };
+     RESULTS["urban&lessthan55&onetothree&no"] = {
+               name: "Hotspot",
+               image: "result_4.jpg",
+               text: "Text for the fourth result"
+             }
+
+    /*************************************************************************
+    If for some reason you need to give your form a different name attribute
+    than "quizform", enter it here. Otherwise, leave this alone.
+
+    *************************************************************************/
+    var FORM_NAME = 'quizform';
+
+    /*************************************************************************
+    DO NOT EDIT ANYTHING BELOW THIS POINT
+
+    *************************************************************************/
+
+    var form = document.forms[FORM_NAME];
+    var form_elements = document.forms[FORM_NAME].elements;
+    var form_element;
+    var result_div = document.getElementById("result_div");
+    var result_image = document.getElementById("result_image");
+    var result_text = document.getElementById("result_text");
+    var result_bbcode = document.getElementById("result_bbcode");
+    var result_html = document.getElementById("result_html");
+    var i;
+
+    var traits = {};
+
+    for (i = 0; i < TRAIT_PAIRS.length; i++) {
+        if (TRAIT_PAIRS[i].length !== 2) {
+            alert("You have more than two traits in a pair. Please ensure that each of the inner sets of square brackets have only two values between them.");
+        }
+        for (var j = 0; j < 2; j++) {
+            traits[TRAIT_PAIRS[i][j]] = true;
+        }
+    }
+
+    function get_trait_combo(index) {
+        var traits = [];
+        for (var i = 0; i < TRAIT_PAIRS.length; i++) {
+            traits.push(TRAIT_PAIRS[i][(index >> i) % 2]);
+        }
+        return traits;
+    }
+
+    for (i = 0; i < Math.pow(2, TRAIT_PAIRS.length); i++) {
+        var result_identifier = get_trait_combo(i).join('&');
+        if (!RESULTS[result_identifier]) {
+            alert("You have not defined a result for " + result_identifier + ". Please double-check your result definitions.");
+        }
+    }
+
+    // Validate the quiz form to prevent common issues
+    for (i = 0; i < form_elements.length; i++) {
+        form_element = form_elements[i];
+        if (form_element.type === "radio" || form_element.type === "checkbox") {
+            if (form_element.value.indexOf('&') !== -1) {
+                alert("Your quiz form has an answer with an ampersand in the value (" + form_element.value + "). This will cause problems. Please change this value.");
+            }
+            else if (!(form_element.value in traits)) {
+                alert("Your quiz form has an answer with the value " + form_element.value + " but this value is not in your trait pairs. You probably made a typo or set your trait pairs up incorrectly.");
+            }
+        }
+    }
+
+    form.onsubmit = function() {
+        var answers = {};
+        var results = [];
+        var first_trait;
+        var second_trait;
+        var joined_results;
+        var result;
+        var i;
+
+        for (var trait in traits) {
+            answers[trait] = 0;
+        }
+
+        for (i = 0; i < form_elements.length; i++) {
+            form_element = form_elements[i];
+            if ((form_element.type === "radio" || form_element.type === "checkbox") && form_element.checked) {
+                answers[form_element.value]++;
+            }
+        }
+
+        for (i = 0; i < TRAIT_PAIRS.length; i++) {
+            first_trait = TRAIT_PAIRS[i][0];
+            second_trait = TRAIT_PAIRS[i][1];
+            if (answers[first_trait] > answers[second_trait]) {
+                results.push(first_trait);
+            }
+            else if (answers[first_trait] < answers[second_trait]) {
+                results.push(second_trait);
+            }
+            else {
+                results.push("both");
+            }
+        }
+
+        joined_results = results.join('&');
+        if (!(joined_results in RESULTS)) {
+            // No specific result
+            for (var i = 0; i < results.length; i++) {
+                if (results[i] === "both") {
+                    results[i] = TRAIT_PAIRS[i][0];
+                }
+            }
+            joined_results = results.join('&');
+        }
+        result = RESULTS[joined_results];
+
+        if (result.link) {
+            window.location = result.link;
+            return false;
+        }
+
+        if (result_image) {
+            result_image.src = RESULTS_FOLDER_URL + result.image;
+            result_image.setAttribute('alt', result.name);
+            if (result.text) result_image.title = result.text;
+        }
+        if (result_text && result.text) {
+            result_text.innerHTML = result.text;
+        }
+        if (result_bbcode) {
+            result_bbcode.innerHTML = '[url=' + QUIZ_URL + '][img]' + RESULTS_FOLDER_URL + result.image + '[/img][/url]';
+        }
+        if (result_html) {
+            result_html.innerHTML = '<a href="' + QUIZ_URL + '"><img src="' + RESULTS_FOLDER_URL + result.image + '" alt="' + result.name + '" title="' + result.text + '"></a>';
+        }
+        result_div.style.display = 'block';
+        return false;
+    };
+})();
+
+
+
+/*************************************************************************
+Free quiz script from The Cave of Dragonflies:
+http://www.dragonflycave.com/free-quiz-scripts
+*************************************************************************/
